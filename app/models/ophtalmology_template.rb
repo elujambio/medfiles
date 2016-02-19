28d0dff3199cb396dbdb607a5bb0b0e7b4fd7a45
Ilse Alejo <ilse.alejo@gmail.com>
2016-02-19 12:37:49 -0600

    Added prescription model for ophtalmology

--- app/models/ophtalmology_template.rb
+++ app/models/ophtalmology_template.rb
@@ -1,5 +1,5 @@
 class OphtalmologyTemplate < ActiveRecord::Base
-	validates :name, :age, :birth_date, :sex, :ocupation, :address, :telephone, :email, presence: true 
+	#validates :name, :age, :birth_date, :sex, :ocupation, :address, :telephone, :email, presence: true 
 	belongs_to :doctor
-	has_many :prescriptions, dependent: :delete_all
+	has_many :prescriptions,:as => :prescriptable, dependent: :delete_all
 end
