9131e4eb6286b6b73bebec2dedd1e72d76951380
Ilse Alejo <ilse.alejo@gmail.com>
2016-03-19 13:15:25 -0600

    Added controller for pregnancy notes. Included method to verify that the doctor that created the template is the one to modify the notes or evolutions

--- app/controllers/pregnancy_notes_controller.rb
+++ app/controllers/pregnancy_notes_controller.rb
@@ -1,17 +1,18 @@
-class PregnancyEvolutionsController < ApplicationController
+class noteNotesController < ApplicationController
 	before_action :authenticate_doctor!
 	before_action :set_gynecology_template
 	before_action :authenticate_doctor_for_gynecology_template
-	before_action :set_pregnancy_evolution, only: [:show, :edit, :update, :destroy]
+	before_action :set_pregnancy_note, only: [:show, :edit, :update, :destroy]
+
 	def new
-		@pregnancy = @gynecology_template.pregnancy_evolutions.build
+		@note = @gynecology_template.pregnancy_notes.build
 	end
 
 	def create
-		@pregnancy = @gynecology_template.pregnancy_evolutions.build(pregnancy_evolutions_params)
-		if @pregnancy.save
+		@note = @gynecology_template.pregnancy_notes.build(pregnancy_notes_params)
+		if @note.save
 			flash[:notice] = "La información se ha guardado correctamente."
-			redirect_to [@gynecology_template, @pregnancy]		
+			redirect_to [@gynecology_template, @note]		
 		else
 			flash[:error] = "No pudimos guardar, vuelve a intentar."
 			render action: "new"
@@ -26,9 +27,9 @@ class PregnancyEvolutionsController < ApplicationController
 	end
 
 	def update
-		if @pregnancy.update(pregnancy_evolutions_params)
+		if @note.update(pregnancy_notes_params)
 			flash[:notice] = "La información se ha guardado correctamente."
-			redirect_to [@gynecology_template, @pregnancy]	
+			redirect_to [@gynecology_template, @note]	
 		else
 			flash[:error] = "No pudimos guardar, vuelve a intentar."
 			render action: "edit"
@@ -37,25 +38,26 @@ class PregnancyEvolutionsController < ApplicationController
 	end
 
 	def destroy
-		@pregnancy.destroy
+		@note.destroy
 		redirect_to @gynecology_template
 	end
 
 	private
-		def pregnancy_evolutions_params
-			params.require(:pregnancy_evolution).permit(:day, :weeks_with_amenorrhea, :blood_pressure, :edema, :uterus_bottom, :fetal_heartbeat, :fetal_movements, :presentation, :pain, :genital_bleeding, :headache, :visual_hearing, :vomit, :urinales, :intestinal, :hb_htc, :glycemia, :albuminuria, :genital_exp, :treatment)
+		def pregnancy_notes_params
+			params.require(:pregnancy_note).permit(:note)
 		end
 
 		def set_gynecology_template
 			@gynecology_template = GynecologyTemplate.find(params[:gynecology_template_id])
 		end
 
-		def set_pregnancy_evolution
-			@pregnancy = @gynecology_template.pregnancy_evolutions.find(params[:id])
+		def set_pregnancy_note
+			@note = @gynecology_template.pregnancy_notes.find(params[:id])
 			rescue  ActiveRecord::RecordNotFound
 			flash[:alert] = "información no encontrada."
 			redirect_to @gynecology_template
 		end
+
 		def authenticate_doctor_for_gynecology_template
 			authenticate_doctor_for_elements(@gynecology_template)
 		end
---
+++ app/helpers/pregnancy_notes_helper.rb
@@ -0,0 +1,2 @@
+module PregnancyNotesHelper
+end
